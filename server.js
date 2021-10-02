8b73fab62540235ff9f5dfbc7c48733038bbec47
malati khawas <malati.khawas12@gmail.com>
2021-10-02 11:43:10 +1000

    server ready

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("the backend api for our ecommerce admin pannel");
+import express from 'express'
+const app = express()
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+const PORT = process.env.PORT || 8000
+
+//middleware
+app.use(helmet())
+app.use(cors())
+app.use(morgan())
+
+app.use('/', (req, res) => {
+  res.send('you have reach the end of the router list')
+})
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log(error)
+  }
+  console.log(`server is running at http://localhost:${PORT}`)
+})
